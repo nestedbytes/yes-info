8fd27465d539b933dd7df4bfa2acdfd3ea04f5a9
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-08-14 17:19:02 +0530

    Made it yes-info and made it whois_url

---
+++ yes-info.py
@@ -0,0 +1,17 @@
+import whois
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def whois_url(url: str):
+    
+
+
+    whois_info = whois.whois(url)
+    print("Domain registrar:", whois_info.registrar)
+    print("WHOIS server:", whois_info.whois_server)
+    print("Domain creation date:", whois_info.creation_date)
+    print("Expiration date:", whois_info.expiration_date)
+    print(whois_info)

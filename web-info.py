681399ed019322d95b3eac05e8c0c102a1ae1432
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-08-14 17:17:31 +0530

    Made cli

--- web-info.py
+++
@@ -1,8 +0,0 @@
-import whois
-domain_name = input("Enter the url:")
-whois_info = whois.whois(domain_name)
-print("Domain registrar:", whois_info.registrar)
-print("WHOIS server:", whois_info.whois_server)
-print("Domain creation date:", whois_info.creation_date)
-print("Expiration date:", whois_info.expiration_date)
-print(whois_info)
